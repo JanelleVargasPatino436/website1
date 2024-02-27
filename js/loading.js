183cfb5b976e8289ff623fd82576f4e9368fdc3d
JanelleVargasPatino436 <142556831+JanelleVargasPatino436@users.noreply.github.com>
2024-02-27 18:09:04 +0000

    Tue, Feb 27, 2024, 10:09 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,10 +3,16 @@ bg = document.querySelector(".bg")
 
 load = 0
 
-loadText.innerText = '${load}%'
-loadText.style.opacity = 1
-bg.style.filter = "blur(10px)"
+int = setInterval(blurring, 30)
+
 
 function blurring() {
     load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = '${load}%'
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = "blur(10px)"
 }
