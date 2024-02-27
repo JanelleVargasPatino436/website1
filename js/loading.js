78d9a13825a9edccf206bd42f55db808096e55dd
JanelleVargasPatino436 <142556831+JanelleVargasPatino436@users.noreply.github.com>
2024-02-27 18:05:33 +0000

    Tue, Feb 27, 2024, 10:05 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,8 +1,10 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector(".bg")
 
 load = 0
 
-loadText.innerText = $(load)%
-loadText.style.opacity = 0.1
+loadText.innerText = '${load}%'
+loadText.style.opacity = 1
+bg.style.filter = "blur(10px)"
 
 
