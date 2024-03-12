8ecfd6b92dc8b32bea197e7d760eed9089c450de
JanelleVargasPatino436 <142556831+JanelleVargasPatino436@users.noreply.github.com>
2024-03-12 17:01:48 +0000

    Tue, Mar 12, 2024, 10:01 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,6 +1,17 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('modal')
+close = document.getElementById('close')
 
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
 })
